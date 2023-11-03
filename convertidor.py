7d5bb336f29a9d46964027564b67017e69fe2202
Camilo <camilogomezr25@gmail.com>
2023-11-03 10:53:37 -0500

    Guardar los frames en la ruta deseada

--- convertidor.py
+++ convertidor.py
@@ -11,3 +11,16 @@ if not captura.isOpened(): #si no se puede habrir o encontrar el video
 carpetaSalida="Frame"
 os.makedirs(carpetaSalida, exist_ok=True) #Se crea una carpeta
 
+cont=0
+while True:
+
+    bandera, frame=captura.read() #leer el video
+
+    if not bandera: #si la bandera es falso es porque no hay mas frame ya que no se puede leer el video
+        break
+
+    nombreFrame=f"{carpetaSalida}/frame_{cont:04d}.jpg" #guarda la ruta y nombre de la imagen
+    cv2.imwrite(nombreFrame, frame) #guarda la imagen donde sele indica la ruta
+
+    cont+=1
+
