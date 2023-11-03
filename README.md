81e9a61466efe5dc7ae6c053a20ed3de4d2021ef
Camilo <camilogomezr25@gmail.com>
2023-11-03 10:34:52 -0500

    Crear el proyecto y añadir la ruta del video

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Video-Convertidor-Imagen
+
+pip install opencv-python ----> Instalar la biblioteca opencv (libre de visión artificial)
---
+++ convertidor.py
@@ -0,0 +1,6 @@
+import cv2
+import os
+
+rutaVideo = os.path.join("video", "video.mp4") #la ruta del video con el nombre del video a convertir
+captura = cv2.VideoCapture(rutaVideo) #captura el video 
+
