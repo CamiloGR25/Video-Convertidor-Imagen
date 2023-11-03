e0b3e47c9a392eebebebfe4964b76cc5813270b0
Camilo <camilogomezr25@gmail.com>
2023-11-03 10:42:49 -0500

    Crear una carpeta frame por python

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Video-Convertidor-Imagen
 
-pip install opencv-python ----> Instalar la biblioteca opencv (libre de visión artificial)
+pip install opencv-python ----> Instalar la biblioteca opencv (visión artificial, procesa img y videos)
--- convertidor.py
+++ convertidor.py
@@ -1,6 +1,13 @@
-import cv2
-import os
+import cv2 #procesar imágenes y videos
+import os #modulo para usar funciones dependientes del sistema operativo
 
 rutaVideo = os.path.join("video", "video.mp4") #la ruta del video con el nombre del video a convertir
 captura = cv2.VideoCapture(rutaVideo) #captura el video 
+
+if not captura.isOpened(): #si no se puede habrir o encontrar el video
+    print("No se puede habrir el video o no existe")
+    exit() #sale del programa
+
+carpetaSalida="Frame"
+os.makedirs(carpetaSalida, exist_ok=True) #Se crea una carpeta
 
